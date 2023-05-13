8b8d6c64e761f84beca1e570ca6a6bc28aef3a04
Yang Chen <yangchen@0587663444.vpn.umich.net>
2023-05-14 00:42:39 +0800

    maha

--- Multi_GP/multi_GP.py
+++ Multi_GP/multi_GP.py
@@ -46,10 +46,27 @@ class MNIST_Net(nn.Module):
         out_list.append(x)
         x = F.dropout(x, training=self.training)
         x = self.fc3(x)
-        out_list.append(x)
-        x = self.fc2(x)    
+        x = self.fc2(x) 
+        out_list.append(x)   
         return F.log_softmax(x, dim = 1), out_list
     
+     
+    def intermediate_forward(self, x, layer_index):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        if layer_index == 1:
+            x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        elif layer_index == 2:
+            x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+            x = x.view(-1, 320)
+            x = F.relu(self.fc1(x))
+        elif layer_index == 2:
+            x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+            x = x.view(-1, 320)
+            x = F.relu(self.fc1(x))
+            x = F.dropout(x, training=self.training)
+            x = self.fc3(x)
+            x = self.fc2(x)    
+        return x
 
 class Fashion_MNIST_Net(nn.Module):
     
@@ -75,7 +92,7 @@ class Fashion_MNIST_Net(nn.Module):
         self.fc2 = nn.Linear(in_features=600, out_features=120)
         self.fc3 = nn.Linear(in_features=120, out_features=16)
         self.fc4 = nn.Linear(in_features=16, out_features=10)
-        self.temperature = temperature
+
         
     def forward(self, x):
         out_list = []
