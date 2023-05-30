a6aa757b98372824e14de002eba6d5d92d6cb36b
Yang Chen <yangchen@0587670542.vpn.umich.net>
2023-05-31 01:49:04 +0800

    maha

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ gpu = 0
 
 
 if __name__ == "__main__":
-    methods = [1]
+    methods = [2, 4]
     
     num_classes = 10
     train_batch_size = 128
@@ -63,8 +63,8 @@ if __name__ == "__main__":
     }
 
 
-    # InD_Datasets = ['MNIST', 'FashionMNIST', 'Cifar_10']
-    InD_Datasets = ['FashionMNIST', 'Cifar_10']
+    InD_Datasets = ['MNIST', 'FashionMNIST', 'Cifar_10']
+    # InD_Datasets = ['FashionMNIST', 'Cifar_10']
 
 
     for InD_Dataset in InD_Datasets:
