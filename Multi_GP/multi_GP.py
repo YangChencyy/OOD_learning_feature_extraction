73caaa8087fcc465bcc4f435158fe813624055f5
Yang Chen <rivachen@umich.edu>
2023-07-04 21:49:17 +0800

    test mul

--- Multi_GP/multi_GP.py
+++ Multi_GP/multi_GP.py
@@ -144,7 +144,7 @@ class Fashion_MNIST_Net(nn.Module):
 def train(network, trloader, epochs, learning_rate = 0.01, momentum = 0.5, verbal = False):
     optimizer = optim.SGD(network.parameters(), lr=learning_rate,
                       momentum=momentum)
-    optimizer = optim.Adam(network.parameters(), lr=0.001)  
+    # optimizer = optim.Adam(network.parameters(), lr=0.001)  
 
 
     network.to(device)
