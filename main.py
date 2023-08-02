b634ca4cc1c278d14fdd9266ed5bfedf8ef5eff2
Yang Chen <rivachen@umich.edu>
2023-08-02 19:13:56 +0800

    cifar

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 
 
     # InD_Datasets = ['MNIST', 'FashionMNIST', 'Cifar_10']
-    InD_Datasets = ['FashionMNIST']
+    InD_Datasets = ['Cifar_10']
 
 
     for InD_Dataset in InD_Datasets:
