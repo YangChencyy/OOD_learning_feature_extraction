f265126c4f645c60a4940e26b9e95fb1e2b0c1cc
Yang Chen <rivachen@umich.edu>
2023-10-25 13:06:41 -0400

    update cifar net

--- Multi_GP/model_cifar.py
+++ Multi_GP/model_cifar.py
@@ -131,142 +131,6 @@ class Bottleneck(nn.Module):
         out = self.relu(out)
 
         return out
-
-
-class ResNet(nn.Module):
-    def __init__(
-        self,
-        block,
-        layers,
-        num_classes=10,
-        zero_init_residual=False,
-        groups=1,
-        width_per_group=64,
-        replace_stride_with_dilation=None,
-        norm_layer=None,
-    ):
-        super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
-
-        self.inplanes = 64
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError(
-                "replace_stride_with_dilation should be None "
-                "or a 3-element tuple, got {}".format(
-                    replace_stride_with_dilation)
-            )
-        self.groups = groups
-        self.base_width = width_per_group
-
-        # CIFAR10: kernel_size 7 -> 3, stride 2 -> 1, padding 3->1
-        self.conv1 = nn.Conv2d(
-            3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False
-        )
-        # END
-
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(
-            block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]
-        )
-        self.layer3 = self._make_layer(
-            block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1]
-        )
-        self.layer4 = self._make_layer(
-            block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        # self.fc1 = nn.Linear(512 * block.expansion, 64) # also try 32
-        # self.fc2 = nn.Linear(64, num_classes)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    m.weight, mode="fan_out", nonlinearity="relu")
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(
-            block(
-                self.inplanes,
-                planes,
-                stride,
-                downsample,
-                self.groups,
-                self.base_width,
-                previous_dilation,
-                norm_layer,
-            )
-        )
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(
-                block(
-                    self.inplanes,
-                    planes,
-                    groups=self.groups,
-                    base_width=self.base_width,
-                    dilation=self.dilation,
-                    norm_layer=norm_layer,
-                )
-            )
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.reshape(x.size(0), -1)
-        x = self.fc(x)
-        # f = self.fc1(x)
-        # x = self.fc2(f)
-
-        return x  # f, x
-        # return f, x
     
 class Cifar_10_Net(nn.Module):
     def __init__(
@@ -279,7 +143,7 @@ class Cifar_10_Net(nn.Module):
         width_per_group=64,
         replace_stride_with_dilation=None,
         norm_layer=None,
-        # temp=None
+        dim_f = 32
     ):
         super(Cifar_10_Net, self).__init__()
         if norm_layer is None:
@@ -322,11 +186,8 @@ class Cifar_10_Net(nn.Module):
             block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
         )
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc1 = nn.Linear(512 * block.expansion, 16) # also try 32
-        self.fc2 = nn.Linear(16, num_classes)
-        # self.log_softmax = nn.LogSoftmax(dim=0)
-        # self.fc3 = nn.Linear(512, num_classes)
-        # self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, num_classes) # also try 32
+        self.fc2 = nn.Linear(512 * block.expansion, dim_f)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -400,24 +261,22 @@ class Cifar_10_Net(nn.Module):
 
         x = self.avgpool(x)
         x = x.reshape(x.size(0), -1)
-        # x = self.fc(x)
-        f = self.fc1(x)
-        x = self.fc2(f)
 
-        # return x  # f, x
-        # return f, self.log_softmax(x)
-        return f, F.log_softmax(x, dim = 1)
+        f = self.fc2(x)
+        x = self.fc(x)
+
+        return f, x # F.log_softmax(x, dim = 1)
 
 
-def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
-    model = ResNet(block, layers, **kwargs)
-    if pretrained:
-        script_dir = os.path.dirname(__file__)
-        state_dict = torch.load(
-            "Multi_GP/checkpoint/" + arch + ".pt", map_location=device
-        )
-        model.load_state_dict(state_dict)
-    return model
+# def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
+#     model = ResNet(block, layers, **kwargs)
+#     if pretrained:
+#         script_dir = os.path.dirname(__file__)
+#         state_dict = torch.load(
+#             "Multi_GP/checkpoint/" + arch + ".pt", map_location=device
+#         )
+#         model.load_state_dict(state_dict)
+#     return model
 
 def load_part(model, state_dict):
     pretrained_dict = state_dict 
