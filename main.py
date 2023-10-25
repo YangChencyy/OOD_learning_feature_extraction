e0cb779d1506c8e0334a58178ee523941516fdd1
Yang Chen <rivachen@umich.edu>
2023-10-25 10:14:17 -0400

    delete other methods

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ from DUQ.evaluate_ood import get_auroc_ood
 from Mahalanobis.OOD_Generate_Mahalanobis import Generate_Maha
 from Mahalanobis.OOD_Regression_Mahalanobis import Regression_Maha
 
-from ODIN.calData import testData_ODIN
-from ODIN.calMetric import metric_ODIN
-from ODIN.densenet import DenseNet3
+# from ODIN.calData import testData_ODIN
+# from ODIN.calMetric import metric_ODIN
+# from ODIN.densenet import DenseNet3
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 
 
     # InD_Datasets = ['MNIST', 'FashionMNIST', 'Cifar_10']
-    InD_Datasets = ['MNIST', 'FashionMNIST']
+    InD_Datasets = ['FashionMNIST']
 
 
     for InD_Dataset in InD_Datasets:
@@ -100,188 +100,77 @@ if __name__ == "__main__":
         print("OOD sets: ", OOD_Dataset)
 
         # multi_GP
-        if 1 in methods:
-            print("Method 1: Multi-GP")
-            print("InD_dataset: ",InD_Dataset)
-            # mkdir directory to save
-            parent_dir = os.getcwd()
-            directory = 'Multi_GP/store_data/' + InD_Dataset
-            path = os.path.join(parent_dir, directory)
-            isExist = os.path.exists(path)
-            if not isExist:
-                # Create a new directory because it does not exist
-                os.makedirs(path)
-                print("The new directory is created!")
-            # Get all labels of training data for GP
-            InD_label = []
-            for i in range(len(train_set)):
-                InD_label.append(train_set.__getitem__(i)[1])
+        print("InD_dataset: ",InD_Dataset)
+        # mkdir directory to save
+        parent_dir = os.getcwd()
+        directory = 'Multi_GP/store_data/' + InD_Dataset
+        path = os.path.join(parent_dir, directory)
+        isExist = os.path.exists(path)
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs(path)
+            print("The new directory is created!")
+        # Get all labels of training data for GP
+        InD_label = []
+        for i in range(len(train_set)):
+            InD_label.append(train_set.__getitem__(i)[1])
 
-            net = None
-            if InD_Dataset == 'Cifar_10':
-                pretrained_resnet18 = resnet18(pretrained=True)
-                net = Cifar_10_Net(BasicBlock, [2, 2, 2, 2])
-                # network.load_sta(torch.load('path'))
-                net = load_part(net, pretrained_resnet18.state_dict())
-    
-                cifar10_train(network = net, trloader = trloader, epochs = 10, optim = 'SGD', verbal=True)
-                torch.save(net, os.path.join(parent_dir, InD_Dataset + "_net.pt"))
+        net = None
+        if InD_Dataset == 'Cifar_10':
+            pretrained_resnet18 = resnet18(pretrained=True)
+            net = Cifar_10_Net(BasicBlock, [2, 2, 2, 2])
+            # network.load_sta(torch.load('path'))
+            net = load_part(net, pretrained_resnet18.state_dict())
 
-            else:
-                epochs = 4
-                net = data_model[InD_Dataset]()
-                train(network = net, trloader = trloader, epochs = epochs, verbal=True)
-                torch.save(net, os.path.join(parent_dir, InD_Dataset + "_net.pt"))
+            cifar10_train(network = net, trloader = trloader, epochs = 10, optim = 'SGD', verbal=True)
+            torch.save(net.state_dict(), os.path.join(parent_dir, InD_Dataset + "_net.pt"))
+            # torch.save(net, os.path.join(parent_dir, InD_Dataset + "_net.pt"))
 
-            ## get InD data for GP
-            InD_features, InD_scores, InD_acc = scores(net, trloader)
-            # test_feature, test_score, test_acc = scores(net, tsloader)
-            print("InD accuracy: ", InD_acc)
-            InD_feature, InD_score = InD_features[0:20000], InD_scores[0:20000]
-            # test_feature, test_score = test_feature[0:5000], test_score[0:5000]
-            test_feature, test_score = InD_features[20000:25000], InD_scores[20000:25000]
-            labels = train_set.targets[20000:25000].numpy().tolist() 
-
-            train_data = np.concatenate((InD_feature.cpu().numpy(), InD_score.cpu().numpy()), 1)
-            train_data = pd.DataFrame(train_data)
-            train_data['label'] = InD_label[0:20000]
-            train_data.to_csv(directory +  '/train.csv')
-            print("train data stored")
-
-            ## get OOD data for GP
-            for i in range(len(OOD_loaders)):
-
-                OOD_feature, OOD_score = scoresOOD(net, OOD_loaders[i])
-                OOD_feature, OOD_score = OOD_feature[0:5000], OOD_score[0:5000]
+        else:
+            epochs = 4
+            net = data_model[InD_Dataset]()
+            train(network = net, trloader = trloader, epochs = epochs, verbal=True)
+            # torch.save(net, os.path.join(parent_dir, InD_Dataset + "_net.pt"))
+            torch.save(net.state_dict(), os.path.join(parent_dir, InD_Dataset + "_net.pt"))
 
 
-                total_CNN = np.concatenate((test_feature.cpu().numpy(), OOD_feature.cpu().numpy()), 0)
-                reducer_CNN = umap.UMAP(random_state = 42, n_neighbors=10, n_components=2)
-                UMAPf = reducer_CNN.fit_transform(total_CNN)
+        ## get InD data for GP
+        InD_features, InD_scores, InD_acc = scores(net, trloader)
+        # test_feature, test_score, test_acc = scores(net, tsloader)
+        print("InD accuracy: ", InD_acc)
+        InD_feature, InD_score = InD_features[0:20000], InD_scores[0:20000]
+        # test_feature, test_score = test_feature[0:5000], test_score[0:5000]
+        test_feature, test_score = InD_features[20000:25000], InD_scores[20000:25000]
+        labels = train_set.targets[20000:25000].numpy().tolist() 
 
-                # all_feature = np.concatenate((test_feature, OOD_feature), 0)
-                all_score = np.concatenate((test_score.cpu().numpy(), OOD_score.cpu().numpy()), 0)
-                DNN_data = np.concatenate((total_CNN, all_score, UMAPf), 1)
+        train_data = np.concatenate((InD_feature.cpu().numpy(), InD_score.cpu().numpy()), 1)
+        train_data = pd.DataFrame(train_data)
+        train_data['label'] = InD_label[0:20000]
+        train_data.to_csv(directory +  '/train.csv')
+        print("train data stored")
 
-                data_df = pd.DataFrame(DNN_data) 
-                data_df['class'] = ['test']*len(test_feature) + ['OOD']*len(OOD_feature)
-                # print(type(labels))
-                data_df['label'] = labels + [10]*len(OOD_feature)
+        ## get OOD data for GP
+        for i in range(len(OOD_loaders)):
 
-                data_df.to_csv(directory +  '/' + OOD_Dataset[i] + '_test.csv')
-                print(OOD_Dataset[i] + " test data stored")
-
-
-        # DUQ
-        if 2 in methods:
-            print("Method 2: DUQ")
-            print("InD_dataset: ",InD_Dataset)
-            if InD_Dataset == 'Cifar_10':
-                for i in range(len(OOD_Dataset)):
-                    print("OOD:", OOD_Dataset[i])
-                    train_model_cifar(train_set, test_set, OOD_sets[i])
-            else:
-                l_gradient_penalties = [0.0]
-                # length_scales = [0.1]
-                length_scales = [0.05, 0.1, 0.2, 0.3, 0.5, 1.0]
-
-                repetition = 1  # Increase for multiple repetitions
-                final_model = False  # set true for final model to train on full train set
-
-                results = {}
-
-                for l_gradient_penalty in l_gradient_penalties:
-                    for length_scale in length_scales:
-                        val_accuracies = []
-                        test_accuracies = []
-                        ood_accuracies = []
-                        roc_aucs_mnist = []
-                        # roc_aucs_notmnist = []
-
-                        for _ in range(repetition):
-                            print(" ### NEW MODEL ### ")
-                            
-                            print("train with parameters:", l_gradient_penalty, length_scale)
-                            model, val_accuracy, test_accuracy = train_model(
-                                l_gradient_penalty, length_scale, final_model, train_set, test_set
-                            )
-                            # accuracy, roc_auc_mnist = get_fashionmnist_mnist_ood(model)
-                            # _, roc_auc_notmnist = get_fashionmnist_notmnist_ood(model)
-
-                            for i in range(len(OOD_Dataset)):
-                                print("OOD: ", OOD_Dataset[i])
-                                accuracy, roc_auc_mnist = get_auroc_ood(test_set, OOD_sets[i], model)
-
-                                val_accuracies.append(val_accuracy)
-                                test_accuracies.append(test_accuracy)
-
-                                ood_accuracies.append(accuracy)
-
-                                roc_aucs_mnist.append(roc_auc_mnist)
-                            #roc_aucs_notmnist.append(roc_auc_notmnist)
-
-                                results[f"lgp{l_gradient_penalty}_ls{length_scale}"] = [
-                                    (np.mean(val_accuracies), np.std(val_accuracies)),
-                                    (np.mean(test_accuracies), np.std(test_accuracies)),
-                                    (np.mean(ood_accuracies), np.std(ood_accuracies)),
-                                    (np.mean(roc_aucs_mnist), np.std(roc_aucs_mnist)),
-                                ]
-                                print(results[f"lgp{l_gradient_penalty}_ls{length_scale}"])
-
-                print(results)
+            OOD_feature, OOD_score = scoresOOD(net, OOD_loaders[i])
+            OOD_feature, OOD_score = OOD_feature[0:5000], OOD_score[0:5000]
 
 
+            total_CNN = np.concatenate((test_feature.cpu().numpy(), OOD_feature.cpu().numpy()), 0)
+            reducer_CNN = umap.UMAP(random_state = 42, n_neighbors=10, n_components=2)
+            UMAPf = reducer_CNN.fit_transform(total_CNN)
 
-        # Mahalanobis
-        if 3 in methods:
-            print("Method 3: Mahalanobis")
-            print("InD_dataset: ",InD_Dataset)
-            parent_dir = os.getcwd()
-            if InD_Dataset == "Cifar_10":
-                net_name = "densenet"
-                net_Maha = torch.load('./Mahalanobis/pre_trained/' + net_name + '_' + InD_Dataset + '.pth', map_location = "cuda:" + str(gpu))
-                print("successfully load model", net_name)
-                
-            else:
-                net_name = "dnn_" + InD_Dataset
-                net_Maha = torch.load(os.path.join(parent_dir, InD_Dataset + "_net.pt"))
-                # net_Maha = data_model[InD_Dataset]()
-            
-            outf = parent_dir + '/Mahalanobis/output/' + net_name  + '/'
-            if os.path.isdir(outf) == False:
-                os.mkdir(outf)
+            # all_feature = np.concatenate((test_feature, OOD_feature), 0)
+            all_score = np.concatenate((test_score.cpu().numpy(), OOD_score.cpu().numpy()), 0)
+            DNN_data = np.concatenate((total_CNN, all_score, UMAPf), 1)
 
-            
-            Generate_Maha(net_Maha, outf, InD_Dataset, OOD_Dataset, trloader, tsloader, 
-                        OOD_loaders, net_name, gpu = gpu, num_classes = 10)
-            Regression_Maha(InD_Dataset, OOD_Dataset, net_name, outf)
+            data_df = pd.DataFrame(DNN_data) 
+            data_df['class'] = ['test']*len(test_feature) + ['OOD']*len(OOD_feature)
+            # print(type(labels))
+            data_df['label'] = labels + [10]*len(OOD_feature)
 
-
-        # ODIN
-        if 4 in methods:
-            print("Method 4: ODIN")
-            print("InD_dataset: ",InD_Dataset)
-            for i in range(len(OOD_sets)):
-                print("OOD: ", OOD_Dataset[i])
-                if InD_Dataset == "Cifar_10":
-                    net_name = "densenet10"
-                    net_ODIN = torch.load("./ODIN/models/{}.pth".format(net_name))
-                    print("successfully load model", net_name)
-                
-                else:
-                    net_ODIN = data_model[InD_Dataset]()
-
-                criterion_ODIN = nn.CrossEntropyLoss()
-
-                tr_l = torch.utils.data.DataLoader(train_set,
-                                                batch_size=1, shuffle=True)
-                ood_l = torch.utils.data.DataLoader(OOD_sets[i],
-                                                batch_size=1, shuffle=True)
-                
-                testData_ODIN(net_ODIN, criterion_ODIN, gpu, tr_l, ood_l, InD_Dataset,
-                            noiseMagnitude1 = 0.0014, temper = 1000)
-                metric_ODIN(InD_Dataset, OOD_sets[i])
-                
+            data_df.to_csv(directory +  '/' + OOD_Dataset[i] + '_test.csv')
+            print(OOD_Dataset[i] + " test data stored")
 
 
     print("\nEND")
