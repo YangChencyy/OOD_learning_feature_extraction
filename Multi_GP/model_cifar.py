4fa635bf0f1fd5caec75427a5200680efb5d7120
Yang Chen <rivachen@umich.edu>
2024-01-19 01:21:54 -0500

    test different umap coefficient

--- Multi_GP/model_cifar.py
+++ Multi_GP/model_cifar.py
@@ -253,25 +253,25 @@ class Cifar_10_Net(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        # x = self.conv1(x)
+        # x = self.bn1(x)
+        # x = self.relu(x)
+        # x = self.maxpool(x)
+
+        # x = self.layer1(x)
+        # x = self.layer2(x)
+        # x = self.layer3(x)
+        # x = self.layer4(x)
+
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        # x1 = self.conv1(x)
-        # x2 = self.bn1(x1)
-        # x3 = self.relu(x2)
-        # x3 = self.maxpool(x3)
-
-        # x3 = self.layer1(x3)
-        # x4 = self.layer2(x3)
-        # x4 = self.layer3(x4)
-        # x = self.layer4(x4)
+        x3 = self.layer1(x)
+        x4 = self.layer2(x3)
+        x4 = self.layer3(x4)
+        x = self.layer4(x4)
 
 
         x = self.avgpool(x)
